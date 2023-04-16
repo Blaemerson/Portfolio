04fba74749934617335093618cf4bb50bf66e810
Blaemerson <blakesavage99@gmail.com>
2023-04-16 10:16:11 -0500

    Add to about page and contact page.

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -16,16 +16,36 @@ const Home: NextPage = () => {
 
             <div className="p-2" />
             <p className="text-slate-800">
-              When I&apos;m not programming, I enjoy the outdoors, literature,
-              and exploring new technologies.
+              I received an Associate&apos;s Degree in Computer Science from Kilgore College in 2019.
+              In 2022, I graduated with a Bachelor&apos;s in Computer Science from the University of Texas at Tyler.
+            </p>
+
+            <div className="p-2" />
+            <p className="text-slate-800">
+              When I&apos;m not developing applications, I enjoy the literature, the outdoors,
+              and spending time with my family.
             </p>
 
             <div className="p-2" />
             <h3>What I Do</h3>
             <div className="p-2" />
             <p className="text-slate-800">
-              As a Software Engineer, I seek to build and maintain complex,
-              multi-faceted applications. In addition to my experience as a
+              As a Software Engineer, I seek to build and maintain
+              web, desktop, and mobile applications.
+            </p>
+            <div className="p-2" />
+            <p className="text-slate-800">
+              I&apos;ve worked in the following positions as a Computer Scientist:
+              <div className="p-1" />
+              <ul className="list-disc ps-10 text-slate-800">
+                <li>Mobile Application Developer - <span className="italic">University of Texas at Tyler (2022)</span></li>
+                <li>Junior Software Engineer - <span className="italic">Kilgore College (2019)</span></li>
+              </ul>
+            </p>
+
+            <div className="p-2" />
+            <p className="text-slate-800">
+              In addition to my experience as a
               software developer and engineer, my peers know me for facing new
               challenges with enthusiasm and a strong commitment to learn.
             </p>
